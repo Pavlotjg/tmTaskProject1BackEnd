e14cfe6f078692226e38c2322d0ec969af541946
Pavlotjg <pavlo.honsevych@gmail.com>
2020-08-31 22:57:06 +0300

    updated User's API

--- database/users.js
+++ database/users.js
@@ -5,10 +5,10 @@ const users = [
     lastName: 'Hatfield',
     email: 'james@gmail.com',
     coordinate: {
-      lat: 57,
-      lng: 78
+      lat: 33.938056,
+      lng: -118.130833
     },
-    avatar: 'https://avatars2.githubusercontent.com/u/53490035?s=60&v=4',
+    avatar: 'https://i7.pngguru.com/preview/909/495/640/james-hetfield-musician-metallica-singer-metallica-thumbnail.jpg',
   },
   {
     id: '2',
@@ -16,10 +16,10 @@ const users = [
     lastName: 'Hammet',
     email: 'kirk@gmail.com',
     coordinate: {
-      lat: 23,
-      lng: 54
+      lat: 37.7749,
+      lng: -122.419
     },
-    avatar: 'https://avatars2.githubusercontent.com/u/53490035?s=60&v=4'
+    avatar: 'https://vignette.wikia.nocookie.net/metallica/images/2/2f/Kirk_22.png/revision/latest?cb=20080816195011'
   },
   {
     id: '3',
@@ -27,10 +27,10 @@ const users = [
     lastName: 'Ulrich',
     email: 'lars@gmail.com',
     coordinate: {
-      lat: 58,
-      lng: 0
+      lat: 55.7490300,
+      lng: 12.5460100
     },
-    avatar: 'https://avatars2.githubusercontent.com/u/53490035?s=60&v=4'
+    avatar: 'https://www4.pictures.gi.zimbio.com/Lars+Ulrich+Special+Screening+Miramax+Great+jhwHT7iApj4l.jpg'
   },
   {
     id: '4',
@@ -38,21 +38,21 @@ const users = [
     lastName: 'Trujillo',
     email: 'rob@gmail.com',
     coordinate: {
-      lat: 78,
-      lng: 12
+      lat: 34.0195,
+      lng: -118.491
     },
-    avatar: 'https://avatars2.githubusercontent.com/u/53490035?s=60&v=4'
+    avatar: 'https://vignette.wikia.nocookie.net/music/images/a/aa/RobertTrujillo.jpg/revision/latest/top-crop/width/360/height/450?cb=20110831020230'
   },
   {
     id: '5',
     firstName: 'Cliff',
     lastName: 'Burton',
-    email: 'cliff@gmail.com',
+    /*email: 'cliff@gmail.com',*/
     coordinate: {
-      lat: 52,
-      lng: 23
+      lat: 37.71,
+      lng: -122.06
     },
-    avatar: 'https://avatars2.githubusercontent.com/u/53490035?s=60&v=4'
+    avatar: 'https://i.pinimg.com/originals/ab/cf/48/abcf4802dd4fe5292f11b8ce2083cc0e.jpg'
   }
 ];
 
